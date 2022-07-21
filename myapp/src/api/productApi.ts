d7abf9bac874b83c5345a32560ea1fad58619484
Do Duc Hieu <hieudd1.bhsoft@gmail.com>
2022-07-21 13:28:18 +0700

    sylas

--- myapp/src/api/productApi.ts
+++ myapp/src/api/productApi.ts
@@ -1,12 +1,10 @@
 import axiosConfig from "./axiosConfig";
 import { ProductType } from "../type";
 
-const URL = "http://localhost:8080";
-
 const userApi = {
     getAll: () => axiosConfig.get("/get-all-product"),
     update: (body: ProductType) => axiosConfig.put(`/update-product`, body),
-    delete: (id: string) => axiosConfig.delete(`${URL}/delete-product/${id}`),
+    delete: (id: string) => axiosConfig.delete(`/delete-product/${id}`),
 };
 
 export default userApi;
--- myapp/src/api/userApi.ts
+++ myapp/src/api/userApi.ts
@@ -1,15 +1,12 @@
 import axiosConfig from "./axiosConfig";
 import { UserType } from "../type";
 
-const URL = "http://localhost:8080";
-
 const userApi = {
-    signUp: (body: UserType) => axiosConfig.post(`${URL}/sign-up`, body),
-    login: (body: UserType) => axiosConfig.post(`${URL}/login`, body),
-    create: (body: UserType) =>
-        axiosConfig.post(`${URL}/create-new-user`, body),
-    update: (body: UserType) => axiosConfig.put(`/update-user`, body),
-    delete: (id: string) => axiosConfig.delete(`${URL}/delete-user/${id}`),
+    signUp: (body: UserType) => axiosConfig.post("/sign-up", body),
+    login: (body: UserType) => axiosConfig.post("/login", body),
+    create: (body: UserType) => axiosConfig.post("/create-new-user", body),
+    update: (body: UserType) => axiosConfig.put("/update-user", body),
+    delete: (id: string) => axiosConfig.delete(`/delete-user/${id}`),
 };
 
 export default userApi;
